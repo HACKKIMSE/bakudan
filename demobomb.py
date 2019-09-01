3b781cf1350d69c72435a8f3d5fa16bfb7e07517
m4zart <swonzy@protonmail.com>
2019-09-01 18:12:39 +0300

    Update demobomb.py

--- demobomb.py
+++ demobomb.py
@@ -16,7 +16,6 @@ except ModuleNotFoundError:
 
 try:
 	os.system('clear')
-	toilet -f mono12 -F border M4ZART
 echo  " "
 	print("Contact Me instagram.com/ichbinharun")
 	no = input("\033[1;37m[?] Enter Number (With Area Code but without '+') =>\033[1;36m ")
