92ab2aa67b33870ab8e05ec62b3d3c0eeff1e8f7
Harun MISTIK <swonzy@protonmail.com>
2019-10-27 15:30:56 +0300

    Update bakudan.sh

--- bakudan.sh
+++ bakudan.sh
@@ -30,7 +30,6 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
-figlet Bakudan DEMO
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border M4ZART
 echo -e "\e[4;34m This Bomber Was Created By M4ZART \e[0m"
